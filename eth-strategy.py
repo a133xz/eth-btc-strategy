1de23f9dc98f377e516c0cc9eeed84baf1c14639
‘Antonio <22479344+a133xz@users.noreply.github.com>
2025-10-07 19:43:56 +0100

    ok

--- eth-strategy.py
+++ eth-strategy.py
@@ -17,7 +17,7 @@ BUY_THRESHOLDS = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
 
 # Date range filter
 # earliest date is 2020-10-08
-START_DATE = "2020-10-08"
+START_DATE = "2025-01-08"
 END_DATE = "2025-10-06"
 
 # Your chosen thresholds
